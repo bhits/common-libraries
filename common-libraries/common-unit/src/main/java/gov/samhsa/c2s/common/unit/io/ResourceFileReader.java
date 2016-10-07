6703dddc10b8b38c00b142da46c157691d3fb9a2
Shruti Rao <shruti.rao@feisystems.com>
2016-10-07 11:14:50 -0400

    Optimize imports and reformat code
    #11132:As a developer, I will have completed CLEAN-UP of 5-9 of 13 Git backend code repositories to make it more deployable by community member (HIE)

--- common-libraries/common-unit/src/main/java/gov/samhsa/c2s/common/unit/io/ResourceFileReader.java
+++ common-libraries/common-unit/src/main/java/gov/samhsa/c2s/common/unit/io/ResourceFileReader.java
@@ -1,29 +1,34 @@
 package gov.samhsa.c2s.common.unit.io;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 
-import org.apache.commons.io.IOUtils;
-
 public class ResourceFileReader {
-	
-	public static String getStringFromResourceFile(String resourceFileUri) {
-		StringWriter writer = new StringWriter();
-		InputStream inputStream = Thread.currentThread()
-				.getContextClassLoader().getResourceAsStream(resourceFileUri);
-		try {
-			IOUtils.copy(inputStream, writer);
-		} catch (IOException e) {
-		}
 
-		String result = writer.toString();
-		try {
-			writer.close();
-		} catch (IOException e) {
-		}
+    private static final Logger logger = LoggerFactory.getLogger(ResourceFileReader.class);
+    public static String getStringFromResourceFile(String resourceFileUri) {
+        StringWriter writer = new StringWriter();
+        InputStream inputStream = Thread.currentThread()
+                .getContextClassLoader().getResourceAsStream(resourceFileUri);
+        try {
+            IOUtils.copy(inputStream, writer);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
 
-		return result;
-	}
+        String result = writer.toString();
+        try {
+            writer.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+
+        return result;
+    }
 
 }
--- common-libraries/common-unit/src/main/java/gov/samhsa/c2s/common/unit/io/SpringBasedResourceFileReader.java
+++ common-libraries/common-unit/src/main/java/gov/samhsa/c2s/common/unit/io/SpringBasedResourceFileReader.java
@@ -1,30 +1,35 @@
 package gov.samhsa.c2s.common.unit.io;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.ClassPathResource;
-
 public class SpringBasedResourceFileReader {
-	
-	public static String getStringFromResourceFile(String resourceFileUri) {
-		StringWriter writer = new StringWriter();
-		ClassPathResource cpr = new ClassPathResource(resourceFileUri);
-		try {
-			InputStream inputStream = cpr.getInputStream();
-			IOUtils.copy(inputStream, writer);
-		} catch (IOException e) {
-		}
 
-		String result = writer.toString();
-		try {
-			writer.close();
-		} catch (IOException e) {
-		}
+    private static final Logger logger = LoggerFactory.getLogger(SpringBasedResourceFileReader.class);
+    public static String getStringFromResourceFile(String resourceFileUri) {
+        StringWriter writer = new StringWriter();
+        ClassPathResource cpr = new ClassPathResource(resourceFileUri);
+        try {
+            InputStream inputStream = cpr.getInputStream();
+            IOUtils.copy(inputStream, writer);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
 
-		return result;
-	}
+        String result = writer.toString();
+        try {
+            writer.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+
+        return result;
+    }
 
 }
